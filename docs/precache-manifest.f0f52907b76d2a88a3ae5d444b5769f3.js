8b85fd29be2f9cd5a5df02d0cb19d7ada52985b2
arioch1980 <arioch1980@hotmail.it>
2020-07-26 17:07:50 +0200

    docs agregado

---
+++ docs/precache-manifest.f0f52907b76d2a88a3ae5d444b5769f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e56e207abb53fe1e9955a3451b5128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7ea1348c427e6df19f",
+    "url": "/static/css/main.ac6646b9.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7ea1348c427e6df19f",
+    "url": "/static/js/main.2e4c8254.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
